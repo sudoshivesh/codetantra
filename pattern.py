406458af70a6e163f8ca8cb79026135df4d18c3d
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2021-04-30 00:11:56 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,22 @@
+#WAP to display the following pattern.
+'''    *
+
+       * * *
+
+       * * * * *
+
+       * * * * * * *
+
+       * * * * * * * * *      '''
+
+n=int(input("Enter n: "))
+space=n-2
+star=1
+for i in range(1,n):
+  for j in range(space):
+    print(' ',end=' ')
+  for k in range(star):
+    print('*',end=' ')
+  space-=1
+  star+=2
+  print()
