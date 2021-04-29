e20445e1f898df7a468c777a0605eb6712dca700
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2021-04-29 23:24:51 +0530

    Create swap.py
    Test Case 1:
    Expected Output:
    Enter·First·Number·36
    Enter·Second·Number·63
    Before·swapping
    a·=·36·,·b·=·63
    After·swapping
    a·=·63·,·b·=·36

---
+++ swap.py
@@ -0,0 +1,7 @@
+a=int(input("Enter First number "))
+b=int(input("Enter First number "))
+print("Before swapping")
+print("a =",a,", b =",b)
+a,b=b,a
+print("After swapping")
+print("a =",a,", b =",b)
