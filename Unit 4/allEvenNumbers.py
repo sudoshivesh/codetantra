954af69755767bbf68652f14f8b263fb5688cc66
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2021-05-03 14:05:20 +0530

    Update allEvenNumbers.py

--- Unit 4/allEvenNumbers.py
+++ Unit 4/allEvenNumbers.py
@@ -1,1 +1,10 @@
-
+#WAP to find min, max and average of elements of a list having numeric data
+a=int(input("How many numbers: "))
+li=[]
+for i in range(a):
+  item=int(input("Enter number "))
+  li.append(item)
+length=len(li)
+print("Maximum element in the list is :",max(li),'')
+print("Minimum element in the list is :",min(li))
+print("Average = ",(sum(li)/length))
