6fc1839374d0f993fb06de9ad9e157647c74925a
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2021-05-03 14:17:01 +0530

    Delete sumOfN.py

--- sumOfN.py
+++
@@ -1,9 +0,0 @@
-#WAP to compute the sum of factorial of the first n natural number.
-term=int(input("Enter number of terms "))
-fact=1
-sum=0
-for i in range(1,term+1):
-  fact=fact*i
-  sum=sum+fact
-print("Sum of series =",sum)
-    
