8eb1fac2603c75244ba7fb6fb808724e24d35fa0
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2021-05-03 14:06:18 +0530

    Create allEvenNumbers2.py

---
+++ Unit 4/allEvenNumbers2.py
@@ -0,0 +1,8 @@
+#WAP to print all even numbers of a list using list comprehension
+mylist=input("Enter elements seperated by a comma:").split(',')
+A=[int(i) for i in mylist]
+li=[]
+for i in A:
+  if i%2==0:
+    li.append(i)
+print("Even numbers in the list: ",li)
