e825a3159c2dfa14e155b5cca2f55af9b2d50997
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2021-05-03 13:56:03 +0530

    Create primes.py

---
+++ Unit 2/primes.py
@@ -0,0 +1,15 @@
+#WAP to display all the prime numbers in given range.
+term1=int(input("Enter first number "))
+term2=int(input("Enter last number "))
+print("Prime numbers from ",term1,"to",term2,"are")
+for i in range(term1,term2+1):
+  if i==4:
+    pass
+  else:
+    flag=0
+    for j in range(2,i):
+      if i%j==0:
+        flag=1
+        break
+  if flag==0:
+    print(i,end=' ')
